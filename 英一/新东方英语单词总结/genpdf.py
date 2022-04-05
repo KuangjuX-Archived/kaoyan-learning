ea352d47f3bedd471ae9d9ab3e0ea3744df84b9b
KuangjuX <18630816527@163.com>
2022-04-05 21:17:21 +0800

    add wordlist2

--- 英一/新东方英语单词总结/genpdf.py
+++ 英一/新东方英语单词总结/genpdf.py
@@ -24,6 +24,10 @@ TEMPLATE_END = r"""
 
 TEMPLATE_WORD = r"""
 \item[{word}] {explanations}
+""" 
+
+TEMPLATE_WORD2 = r"""
+\item[{word}]
 """
 
 TEMPLATE_JUST_WORD = r"""
@@ -32,26 +36,36 @@ TEMPLATE_JUST_WORD = r"""
 
 SRC_WORDLIST = 'WordList.csv'
 DST_WORDLIST = 'wordlist.tex'
+DST_WORDLIST2 = 'wordlist2.tex'
 wordlist = open(DST_WORDLIST, 'w', encoding='utf-8')
+wordlist2 = open(DST_WORDLIST2, 'w', encoding='utf-8')
+
 wordlist.write(TEMPLATE_BEGIN)
+wordlist2.write(TEMPLATE_BEGIN)
 
 with open(SRC_WORDLIST, 'r', encoding='UTF-8') as f:
     reader = csv.reader(f)
     for index, row in enumerate(reader):
         if len(row) > 1 and row[0] != '英文':
-            fmt_args = {'word': row[0], 'explanations': row[1]}
-            wordlist.write(TEMPLATE_WORD.format(**fmt_args))
-            print(fmt_args)
+            fmt_args_1 = {'word': row[0], 'explanations': row[1]}
+            fmt_args_2 = {'word': row[0]}
+            wordlist.write(TEMPLATE_WORD.format(**fmt_args_1))
+            wordlist2.write(TEMPLATE_WORD2.format(**fmt_args_2))
 
 wordlist.write(TEMPLATE_END)
+wordlist2.write(TEMPLATE_END)
 
 os.system("xelatex -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape wordlist.tex")
-
+os.system("xelatex -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape wordlist2.tex")
 
 remove_files = [
     "wordlist.log",
     "wordlist.aux",
-    "wordlist.synctex.gz"
+    "wordlist.synctex.gz",
+    "wordlist2.log",
+    "wordlist2.aux",
+    "wordlist2.synctex.gz",
+
 ]
 
 for file in remove_files:
